d02244bbbcfe83fa306bffd9f6600e83684a0bd3
Serhii Chaplia <serg.chaplya@gmail.com>
2018-06-21 12:58:01 +0300

    Update checkmail.go

--- checkmail.go
+++ checkmail.go
@@ -52,10 +52,10 @@ func ValidateHost(email string) error {
 	}
 
 	client, err := smtp.Dial(fmt.Sprintf("%s:%d", mx[0].Host, 25))
-	defer client.Close()
 	if err != nil {
 		return NewSmtpError(err)
 	}
+	defer client.Close()
 
 	t := time.AfterFunc(forceDisconnectAfter, func() { client.Close() })
 	defer t.Stop()
